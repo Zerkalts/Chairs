c800d47b4783c760c277f69b7288123a2e4e097f
Zerkalts <60142629+Zerkalts@users.noreply.github.com>
2020-02-11 15:30:41 +0100

    Update chairs_per_person.py

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,18 @@
-f = open("reservations.csv")
+f = open("reservations")
 
+List = []
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
+    try :
+        chairs_per_person = 50 / int(number)
+    except ValueError:
+        if number.strip("\n") not in List:
+            List.append(number.strip("\n"))
+        pass
+    except ZeroDivisionError:
+        if number.strip("\n") not in List:
+            List.append(number.strip("\n"))
+        pass
+
     print("{} will get {} chairs per person".format(name, chairs_per_person))
+print(List)
